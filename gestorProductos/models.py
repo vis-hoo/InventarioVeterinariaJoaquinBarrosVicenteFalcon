2870b6bc42f40632ad46a356f91c359698948a4e
vis-hoo <vicente.falcon.arancibia@hotmail.com>
2025-11-27 13:30:55 -0300

    Creados los modelos de Producto y Categoría.

--- gestorProductos/models.py
+++ gestorProductos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    descripcion = models.TextField()
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    descripcion = models.TextField()
+    precio = models.IntegerField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
